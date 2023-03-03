414f96ccc1a583991a9371857c64554f1387f28b
Yazgeldi <yazgeldi20091984@gmail.com>
2023-03-03 19:09:47 +0300

    init template

---
+++ src/js/main.js
@@ -0,0 +1,181 @@
+// RESPONSIVE
+
+// Breakpoints
+const breakpoints = {
+  xl: 1200,
+  lg: 992,
+  md: 768,
+  sm: 576,
+  xsm: 375,
+};
+
+// Media quires
+const MQ = {
+  wWidth: 0,
+  isXL: false,
+  isLG: false,
+  isMD: false,
+  isSM: false,
+  isXSM: false,
+  updateState: function () {
+    this.wWidth = $(window).width();
+
+    for (let key in breakpoints) {
+      this['is' + key.toUpperCase()] = this.wWidth <= breakpoints[key];
+    }
+  },
+};
+
+MQ.updateState();
+
+$(document).ready(function () {
+  //
+});
+
+$(window).on('load', function () {
+  //
+});
+
+$(window).on('resize', function () {
+  MQ.updateState();
+});
+
+// COMMON FUNCTIONS
+
+// Popup opener
+$('.js-popup').on('click', function (event) {
+  event.preventDefault();
+  let popupID = $(this).attr('href');
+
+  mfpPopup(popupID);
+});
+
+// Mobile menu toggle
+$('.js-menu').on('click',function () {
+  $(this).toggleClass('is-active');
+  $('.menu').toggleClass('is-opened');
+});
+
+// Phone input mask
+$('input[type="tel"]').inputmask({
+  mask: '+7 (999) 999-99-99',
+  showMaskOnHover: false,
+});
+
+// E-mail Ajax Send
+$('form').on('submit',function (e) {
+  e.preventDefault();
+
+  let form = $(this);
+  let formData = {};
+  formData.data = {};
+
+  // Serialize
+  form.find('input, textarea').each(function () {
+    let name = $(this).attr('name');
+    let title = $(this).attr('data-name');
+    let value = $(this).val();
+
+    formData.data[name] = {
+      title: title,
+      value: value,
+    };
+
+    if (name === 'subject') {
+      formData.subject = {
+        value: value,
+      };
+      delete formData.data.subject;
+    }
+  });
+
+  $.ajax({
+    type: 'POST',
+    url: 'mail/mail.php',
+    dataType: 'json',
+    data: formData,
+  }).done(function (data) {
+    if (data.status === 'success') {
+      if (form.closest('.mfp-wrap').hasClass('mfp-ready')) {
+        form.find('.form-result').addClass('form-result--success');
+      } else {
+        mfpPopup('#success');
+      }
+
+      setTimeout(function () {
+        if (form.closest('.mfp-wrap').hasClass('mfp-ready')) {
+          form.find('.form-result').removeClass('form-result--success');
+        }
+        $.magnificPopup.close();
+        form.trigger('reset');
+      }, 3000);
+    } else {
+      alert('Ajax result: ' + data.status);
+    }
+  });
+  return false;
+});
+
+const mfpPopup = function (popupID, source) {
+  // https://dimsemenov.com/plugins/magnific-popup/
+  $.magnificPopup.open({
+    items: { src: popupID },
+    type: 'inline',
+    fixedContentPos: false,
+    fixedBgPos: true,
+    overflowY: 'auto',
+    closeBtnInside: true,
+    preloader: false,
+    midClick: true,
+    removalDelay: 300,
+    closeMarkup: '<button type="button" class="mfp-close">&times;</button>',
+    mainClass: 'mfp-fade-zoom',
+    // callbacks: {
+    // 	open: function() {
+    // 		$('.source').val(source);
+    // 	}
+    // }
+  });
+};
+
+// $(document).ready(function() {
+//   $('.header__cities, .header__arrow').click(function() {
+//     $('.header__modal').toggle();
+//   });
+  
+//   $('.header_modal li').click(function() {
+//     var city = $(this).text();
+//     $('.header__cities').text(city);
+//     $('.header__modal').hide();
+//   });
+// });
+
+
+$(document).ready(function() {
+  $('.header__arrow').click(function() {
+    $('.header__modal').toggle();
+  });
+  
+  $('.header_modal li').click(function() {
+    var city = $(this).text();
+    $('.header__arrow').text(city);
+    $('.header__modal').hide();
+  });
+});
+
+
+var searchImage = document.querySelector(".header-main__search");
+var searchBox = document.querySelector(".search-box");
+var isSearchVisible = false;
+
+searchImage.addEventListener("click", function() {
+  if (isSearchVisible) {
+    searchBox.style.display = "none";
+    isSearchVisible = false;
+  } else {
+    searchBox.style.display = "block";
+    isSearchVisible = true;
+  }
+});
+
+
