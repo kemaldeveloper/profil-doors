501a4f14786e0a457737856fd5a40bdd284df571
Kemal <gurbansahadowkemal01@gmail.com>
2023-03-17 21:24:34 +0300

    start card-product block

--- src/js/main.js
+++ src/js/main.js
@@ -259,9 +259,6 @@ $('.tabs__items').on('click', 'li:not(.active)', function () {
     .addClass('active');
 });
 
-
-
-
 // Анимация плавного появления и исчезновения блоков
 //  с помощью методов fadeIn() и fadeOut():
 
@@ -274,9 +271,7 @@ $('.tabs__items').on('click', 'li:not(.active)', function () {
 //   $(this).addClass('active').siblings().removeClass('active');
 // });
 
-
-
-// нимация появления и исчезновения блоков с помощью 
+// нимация появления и исчезновения блоков с помощью
 // методов slideDown() и slideUp():
 
 // $('.tabs__items').on('click', 'li:not(.active)', function () {
@@ -288,18 +283,14 @@ $('.tabs__items').on('click', 'li:not(.active)', function () {
 //   $(this).addClass('active').siblings().removeClass('active');
 // });
 
-
-// Анимация появления и исчезновения блоков с помощью методов 
+// Анимация появления и исчезновения блоков с помощью методов
 // animate() и opacity():
 
 // $('.tabs__items').on('click', 'li:not(.active)', function () {
 //   let index = $(this).index();
-//   $('.tabs__block.active').animate({opacity: 0}, 300, function () {
+//   $('.tabs__block.active').animate({ opacity: 0 }, 300, function () {
 //     $(this).removeClass('active');
-//     $('.tabs__block').eq(index).animate({opacity: 1}, 300).addClass('active');
+//     $('.tabs__block').eq(index).animate({ opacity: 1 }, 300).addClass('active');
 //   });
 //   $(this).addClass('active').siblings().removeClass('active');
 // });
-
-
-
